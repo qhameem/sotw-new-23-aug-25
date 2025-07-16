4958408d3228e2c1b461980d59e06ecf3b94fda6
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-07-16 12:24:34 +0600

    Login with Google button emphasis

--- resources/views/auth/partials/google-login-button.blade.php
+++ resources/views/auth/partials/google-login-button.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('auth.google') }}"
-   class="flex items-center justify-center w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-    <img class="h-6 w-6 mr-2" src="https://www.svgrepo.com/show/475656/google-color.svg"
+   class="flex items-center justify-center w-full px-4 py-3 border-2 rounded-md shadow-sm text-base font-medium text-gray-700 bg-white hover:bg-neutral-50">
+    <img class="h-7 w-7 mr-2" src="https://www.svgrepo.com/show/475656/google-color.svg"
          alt="Google logo">
     Sign in with Google
 </a>
