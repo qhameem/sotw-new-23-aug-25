515e996187e022b324143c37e34d63ca496a74ea
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-07-16 13:02:21 +0600

    add-product-button change

--- resources/views/components/add-product-button.blade.php
+++ resources/views/components/add-product-button.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('products.create') }}"
    class="bg-white hover:bg-rose-50 text-primary-500 border border-primary-500 text-sm font-semibold py-1 px-3 rounded-md transition duration-300 shadow">
     <span class="hidden lg:inline">Add your product &rarr;</span>
-    <span class="lg:hidden">Submit &rarr;</span>
+    <span class="lg:hidden md:hidden">Submit &rarr;</span>
 </a>
