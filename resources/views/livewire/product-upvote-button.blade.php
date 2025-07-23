61a78afd95859ff00ce37ba98547b82134442a2f
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-07-23 23:56:54 +0600

    Upvote button redesign

--- resources/views/livewire/product-upvote-button.blade.php
+++ resources/views/livewire/product-upvote-button.blade.php
@@ -1,9 +1,17 @@
-<button type="button"
-        wire:click="toggleUpvote"
-        class="inline-flex items-center px-4 py-1 border rounded-md shadow-sm text-sm font-medium text-white bg-gray-800 hover:bg-gray-900 focus:outline-none border-transparent">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
-    </svg>
-    Upvote
-    <span class="ml-2 pl-2 border-l border-gray-600">{{ $votesCount }}</span>
-</button>
+<div x-data="upvote(
+    {{ $product->is_upvoted_by_current_user ? 'true' : 'false' }},
+    {{ $product->votes_count ?? 0 }},
+    '{{ $product->id }}',
+    '{{ $product->slug }}',
+    {{ Auth::check() ? 'true' : 'false' }},
+    '{{ csrf_token() }}'
+)" class="">
+ 
+    <button type="button"
+            @click="toggleUpvote"
+            class="flex flex-col items-center justify-center w-12 aspect-square bg-white border border-gray-200 rounded-md shadow-sm text-xs text-gray-500 focus:outline-none">
+        <svg class="h-4 w-4 fill-gray-300" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0" ></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19z"></path></g></svg>
+        <span x-text="votesCount" class="text-gray-800"></span>
+    </button>
+    <p x-show="errorMessage" x-text="errorMessage" class="text-red-500 text-xs mt-1"></p>
+</div>
