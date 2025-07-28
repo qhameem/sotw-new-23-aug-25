b1e50ce13659ca2e0e1db224d68c5e54329d830c
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-07-29 01:31:29 +0600

    changed product page upvote button design

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -20,14 +20,14 @@
         <div class="md:col-span-2">
             <div class="flex items-center mb-4">
                 @if($product->logo)
-                    <img src="{{ Str::startsWith($product->logo, 'http') ? $product->logo : asset('storage/' . $product->logo) }}" alt="{{ $product->name }} logo" class="w-16 h-16 object-contain rounded-lg mr-4">
+                    <img src="{{ Str::startsWith($product->logo, 'http') ? $product->logo : asset('storage/' . $product->logo) }}" alt="{{ $product->name }} logo" class="size-20 object-contain rounded-lg mr-3">
                 @elseif($product->link)
-                    <img src="{{ 'https://www.google.com/s2/favicons?sz=64&domain_url=' . urlencode($product->link) }}" alt="{{ $product->name }} favicon" class="w-16 h-16 object-contain rounded-lg mr-4">
+                    <img src="{{ 'https://www.google.com/s2/favicons?sz=64&domain_url=' . urlencode($product->link) }}" alt="{{ $product->name }} favicon" class="size-20 object-contain rounded-lg mr-3">
                 @endif
                 <div>
                     <h1 class="text-2xl font-bold text-gray-900">{{ $product->name }}</h1>
-                    <p class="text-gray-600">{{ $product->product_page_tagline }}</p>
-                    <div class="flex flex-wrap items-center mt-2">
+                    <p class="text-gray-800 text-base">{{ $product->product_page_tagline }}</p>
+                    <div class="flex flex-wrap items-center mt-1">
                         @php
                             $generalCategories = $product->categories->filter(function ($cat) {
                                 return !$cat->types->contains('name', 'Pricing');
@@ -46,18 +46,30 @@
             <div class="flex items-center justify-between mb-6">
                 <div class="flex items-center">
                     @unless($product->user->hasRole('admin'))
-                        <img src="{{ $product->user->avatar() }}" alt="{{ $product->user->name }}" class="w-8 h-8 rounded-full mr-3">
-                        <span class="text-gray-700">{{ $product->user->name }}</span>
+                    <div class="flex flex-col gap-1">
+                        <div class="text-xs font-medium">
+                           Publisher
+                        </div>
+                        <div class="flex flex-row">
+                            <div>
+                                 <img src="{{ $product->user->avatar() }}" alt="{{ $product->user->name }}" class="size-5 rounded-full mr-1 border">
+                          </div>
+                            <div class="text-gray-700 text-xs content-center"> 
+                                {{ $product->user->name }}
+                            </div>
+                            
+                        </div>
+                    </div>
+                        
+                       
                     @endunless
                 </div>
-                <div class="flex items-center space-x-2">
+                <div class="flex items-center space-x-4">
                     <a href="{{ $product->link . (strpos($product->link, '?') === false ? '?' : '&') }}utm_source=softwareontheweb.com"
                        target="_blank" rel="noopener nofollow noreferrer"
-                       class="inline-flex items-center px-4 py-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
-                        Visit
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                        </svg>
+                       class="inline-flex items-center px-4 py-1.5 text-sm font-semibold text-gray-700 bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+                        Visit Website &nbsp;
+                        <svg class="size-4 stroke-gray-600" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 17L17 7M17 7H8M17 7V16" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                     </a>
                     @livewire('product-upvote-button', ['product' => $product])
                 </div>
