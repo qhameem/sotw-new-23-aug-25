16ffc11d11e54e09d71b4662339cf67e97d2373d
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-07-28 14:47:45 +0600

    mobile logo

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,7 +1,10 @@
 <div class="sticky top-0 z-40 border-b bg-white border-gray-200">
     <div class="flex justify-between items-center px-4 py-[0.78rem]">
         <div>
-            <h1 class="text-lg md:text-base font-semibold tracking-tight">{{ $title }}</h1>
+            <div class="flex items-center">
+                <img src="{{ asset('favicon/logo.svg') }}" alt="Favicon" class="mobile-favicon mr-2 w-10 h-10 md:hidden">
+                <h1 class="text-base md:text-base font-semibold tracking-tight">{{ $title }}</h1>
+            </div>
         </div>
         <div class="flex items-center space-x-4">
             @if (isset($actions))
@@ -10,6 +13,15 @@
             <div class="md:hidden">
                 <x-user-dropdown />
             </div>
+            @push('styles')
+            <style>
+                .mobile-favicon {
+                    width: 16px;
+                    height: 16px;
+                    vertical-align: middle;
+                }
+            </style>
+            @endpush
         </div>
     </div>
     <hr class="">
