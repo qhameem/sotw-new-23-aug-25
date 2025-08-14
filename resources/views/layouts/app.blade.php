c6aced8480350a7eaa8e874064957fa814d345a0
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-08-14 15:58:13 +0600

    Meta tag issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,8 +43,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', config('app.name', 'Laravel'))</title>
-    <meta name="description" content="@yield('meta_description', '')">
+    <title>@yield('title', $meta_title ?? config('app.name', 'Laravel'))</title>
+    <meta name="description" content="@yield('meta_description', $meta_description ?? '')">
 
     @if(config('theme.font_url'))
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
