4e6061e24e3cc39d832083817dee318013b0dae5
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-08-22 19:56:19 +0600

    Product add form UI/UX

--- resources/views/products/partials/_form.blade.php
+++ resources/views/products/partials/_form.blade.php
@@ -5,14 +5,17 @@
 
 <div class="space-y-6">
     <!-- Product URL -->
-    <div class="grid md:grid-cols-4 gap-4 items-start">
-        <div class="md:col-span-1">
-            <label class="block font-semibold md:text-left md:pr-4" for="product_url">Product URL<span class="text-red-500 ml-1"><span class="text-red-500 ml-1">*</span></span></label>
+    <div class="grid md:grid-cols-3 gap-1 items-start rounded-2xl border border-slate-200 p-4 bg-gradient-to-t from-white to-stone-100">
+        
+        <div class="md:col-span-2">
+            <div class="font-semibold">Autofill details</div>
+            <div class="text-xs">Just input your product URL, and we'll automatically fill in the details for you!</div>
+            <label class="block text-xs font-semibold md:text-left mt-2" for="product_url">Product URL<span class="text-red-500 ml-1"><span class="text-red-500">*</span></span></label>
            
         </div>
         <div class="md:col-span-3">
             <div class="flex items-center relative">
-                <input type="url" id="product_url" name="link" x-model="link" value="{{ old('link', $product->link ?? '') }}" @input.debounce.500ms="checkUrlUnique" @blur="checkUrlUnique" @keydown.enter.prevent="checkUrlUnique" class="w-full border border-gray-300 rounded-md px-3 py-2 pr-10 text-sm focus:border-primary-500 focus:ring-primary-500 placeholder:text-xs placeholder-gray-400" placeholder="Paste product link here" required>
+                <input type="url" id="product_url" name="link" x-model="link" value="{{ old('link', $product->link ?? '') }}" @input.debounce.500ms="checkUrlUnique" @blur="checkUrlUnique" @keydown.enter.prevent="checkUrlUnique" class="w-full border border-gray-300 rounded-md px-3 py-2 pr-10 text-sm focus:border-primary-500 focus:ring-primary-500 placeholder:text-sm placeholder-gray-400" placeholder="https://" required>
                 <template x-if="checkingUrl && !isEditMode">
                     <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                         <svg class="animate-spin h-5 w-5 text-rose-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -25,7 +28,7 @@
             <template x-if="urlExists && !checkingUrl && !isEditMode">
                 <div class="text-red-600 text-sm mt-1">This URL is already listed. Please enter a different product URL.</div>
             </template>
-            <div class="mt-1 h-6 flex items-center"> <!-- Set a fixed height and flex properties -->
+            <!-- <div class="mt-1 h-6 flex items-center"> 
                 <span x-show="!loadingMeta" class="text-xs text-gray-600 tracking-tight">&#128161; Paste your product link — we’ll fetch the details automatically</span>
                 <div x-show="loadingMeta" class="text-sm text-gray-600 flex items-center" style="display: none;">
                     <svg class="animate-spin h-5 w-5 mr-2 text-rose-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -34,7 +37,7 @@
                     </svg>
                     Fetching product details...
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 
