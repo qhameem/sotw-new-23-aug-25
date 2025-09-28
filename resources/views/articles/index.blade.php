e24c92faf94037c93a41555ebd712d2f00eb8b7e
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-09-28 15:45:32 +0600

    write article button placement and icon

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,21 +3,20 @@
 @section('title', 'Articles | Software on the Web')
 
 @section('header-title')
-    <h1 class="text-xl font-bold text-gray-800">{{ __('Articles') }}</h1>
+    <div class="flex items-center space-x-4">
+        <h1 class="text-xl font-bold text-gray-800">{{ __('Articles') }}</h1>
+        @auth
+            <a href="{{ route('articles.create') }}" class="inline-flex items-center px-4 py-1 border border-gray-300 rounded-lg font-medium text-sm text-gray-600 hover:bg-gray-50 active:bg-gray-900 focus:outline-none disabled:opacity-25 transition ease-in-out duration-150">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                </svg>
+                {{ __('Write article') }}
+            </a>
+        @endauth
+    </div>
 @endsection
 
 @section('actions')
-    @auth
-    <div class="flex items-center space-x-2">
-        <x-add-product-button />
-        <a href="{{ route('articles.create') }}" class="inline-flex items-center px-4 py-1 bg-gray-800 border border-transparent rounded-lg font-medium text-sm text-white hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-            </svg>
-            {{ __('Write Article') }}
-        </a>
-    </div>
-    @endauth
 @endsection
 
 @section('content')
