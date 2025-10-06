fb29f51e6093d2d75758739812d00d82aab3e5c7
Quazi Hameem Mahmud <qhameemb@gmail.com>
2025-10-06 22:55:15 +0600

    error resolve attmept 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -161,14 +161,6 @@
       data-is-authenticated="{{ Auth::check() ? '1' : '0' }}"
       data-login-url="{{ route('login') }}"
       data-csrf-token="{{ csrf_token() }}">
-    @php
-        $bodySnippets = \App\Models\CodeSnippet::where('location', 'body')->get();
-    @endphp
-    @foreach ($bodySnippets as $snippet)
-        @if ($snippet->page === 'all' || $snippet->page === $page)
-            {!! $snippet->code !!}
-        @endif
-    @endforeach
 
     <x-main-content-layout :main-content-max-width="$mainContentMaxWidth ?? 'max-w-3xl'">
         <x-slot:title>
