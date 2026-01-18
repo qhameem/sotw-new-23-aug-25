91b5ffbe2ea581877eca62d9346ce1aabda011de
Quazi Hameem Mahmud <qhameemb@gmail.com>
2026-01-19 01:55:05 +0600

    updated app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -188,15 +188,7 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     @stack('styles')
 
-    <script type="application/ld+json">
-    {
-      "@context": "https://schema.org",
-      "@type": "WebSite",
-      "name": "Software on the Web",
-      "alternateName": ["Softwareontheweb"],
-      "url": "https://softwareontheweb.com"
-    }
-    </script>
+
     @php
         $headSnippets = \App\Models\CodeSnippet::where('location', 'head')->get();
         $page = \Illuminate\Support\Facades\Route::currentRouteName();
