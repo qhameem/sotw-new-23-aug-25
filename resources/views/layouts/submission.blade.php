c778987a3d73f89a88c3d7e8449595ae97ca0288
Quazi Hameem Mahmud <qhameemb@gmail.com>
2026-01-19 01:41:20 +0600

    make Google show "Software on the Web" as your site name instead of the domain name

--- resources/views/layouts/submission.blade.php
+++ resources/views/layouts/submission.blade.php
@@ -188,16 +188,15 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     @stack('styles')
 
-    @verbatim
-        <script type="application/ld+json">
-                                    {
-                                      "@context": "https://schema.org",
-                                      "@type": "WebSite",
-                                      "name": "Software on the Web",
-                                      "url": "https://www.softwareontheweb.com"
-                                    }
-                                    </script>
-    @endverbatim
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "WebSite",
+      "name": "Software on the Web",
+      "alternateName": ["Softwareontheweb"],
+      "url": "https://softwareontheweb.com"
+    }
+    </script>
     @php
         $headSnippets = \App\Models\CodeSnippet::where('location', 'head')->get();
         $page = \Illuminate\Support\Facades\Route::currentRouteName();
