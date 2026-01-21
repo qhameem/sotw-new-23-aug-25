d04af98e9b7acce2e5edb88c1c18b14b8193d068
Quazi Hameem Mahmud <qhameemb@gmail.com>
2026-01-21 10:48:41 +0600

    log in page redesign

--- resources/views/auth/partials/google-login-button.blade.php
+++ resources/views/auth/partials/google-login-button.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('auth.google') }}"
-   class="flex items-center justify-center w-full px-4 py-3 border-2 rounded-md shadow-sm text-base font-medium text-gray-700 bg-white hover:bg-neutral-50">
+   class="flex items-center justify-center w-full px-4 py-3 border border-gray-500 rounded-full text-base font-medium text-gray-700 bg-white hover:bg-neutral-50">
     <img class="h-7 w-7 mr-2" src="https://www.svgrepo.com/show/475656/google-color.svg"
          alt="Google logo">
-    Sign in with Google
+    Continue with Google
 </a>
