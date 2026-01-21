4ee243ee098acd09d675a9dd3b8396d336dfdf9a
Quazi Hameem Mahmud <qhameemb@gmail.com>
2026-01-21 11:03:14 +0600

    sign up sign in modal change

--- resources/views/auth/partials/google-login-button.blade.php
+++ resources/views/auth/partials/google-login-button.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('auth.google') }}"
-   class="flex items-center justify-center w-full px-4 py-3 border border-gray-500 rounded-full text-base font-medium text-gray-700 bg-white hover:bg-neutral-50">
+   class="flex items-center justify-center w-full px-4 py-3 border border-gray-500 rounded-full text-base font-medium text-gray-700 bg-white hover:bg-gray-800 hover:text-white">
     <img class="h-7 w-7 mr-2" src="https://www.svgrepo.com/show/475656/google-color.svg"
          alt="Google logo">
     Continue with Google
--- resources/views/auth/partials/login-form.blade.php
+++ resources/views/auth/partials/login-form.blade.php
@@ -16,18 +16,22 @@
     </div>
     <div class="block mt-4">
         <label for="remember_me_modal_login" class="inline-flex items-center">
-            <input id="remember_me_modal_login" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-            <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            <input id="remember_me_modal_login" type="checkbox" class="rounded border-gray-300 text-rose-600 shadow-sm focus:ring-rose-500" name="remember">
+            <span class="ms-2 text-xs text-gray-600">{{ __('Remember me') }}</span>
         </label>
     </div>
     <div class="flex items-center justify-end mt-4">
         @if (Route::has('password.request'))
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+            <a class="text-xs text-gray-400 hover:text-gray-600 hover:underline rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                 {{ __('Forgot your password?') }}
             </a>
         @endif
-        <x-primary-button class="ms-3">
+        <!-- <x-primary-button class="ms-3">
             {{ __('Log in') }}
-        </x-primary-button>
+        </x-primary-button> -->
+           <span class="ms-3 text-sm text-gray-500 border border-gray-400 rounded-md px-4 py-1 
+           hover:cursor-pointer hover:text-white hover:bg-gray-800">
+            {{ __('Log in') }}
+        </span>
     </div>
 </form>
