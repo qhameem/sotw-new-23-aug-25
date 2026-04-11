839759fa3098ccd83378db52e8ac2163489dba31
Quazi Hameem Mahmud <qhameemb@gmail.com>
2026-04-11 13:11:01 +0600

    update to what is sotw card

--- resources/views/partials/_what-is-sotw-card.blade.php
+++ resources/views/partials/_what-is-sotw-card.blade.php
@@ -1,10 +1,14 @@
 <div class="p-4 bg-gradient-to-tr from-white to-sky-50 tracking-tight border rounded-lg text-center">
     <h3 class="text-lg px-2 font-base text-gray-700 font-noto-serif tracking-tighter">
-    What is <span class="italic">Software on the web?</span></h3>
+    What is <span class="italic">Software on the Web?</span></h3>
     <p class="mt-2 px-2 text-sm text-gray-600">
-        Software on the web is a daily curation and launch platform for software products. We handpick each product. 
+        Software on the Web is a weekly curation and launch platform for software products. We handpick each tool to ensure quality and utility. 
         <br><br>
-        Explore exciting new products, find fresh inspiration, and showcase your own product to thousands of potential customers.
+        <h3 class="text-lg px-2 font-base text-gray-700 font-noto-serif tracking-tighter">For the <span class="italic">curious:</span></h3> 
+        <p class="mt-2 px-2 text-sm text-gray-600">We do the digging for you. Whether you need a tool for a specific task or a niche app you can't find anywhere else, our detailed listings help you find exactly what you need.</p>
+        <br>
+        <h3 class="text-lg px-2 font-base text-gray-700 font-noto-serif tracking-tighter">For the <span class="italic">builders:</span></h3> 
+        <p class="mt-2 px-2 text-sm text-gray-600">Showcase your hard work. Launch your product on our platform and get your tool in front of thousands of potential customers.</p>
     </p>
     <div class="mt-4 px-2">
         <a href="#" @click.prevent="$dispatch('open-modal', { name: 'login-required-modal' })" class="text-sm bg-gray-900 text-white py-1 px-4 rounded-lg font-semibold">Sign up <span aria-hidden="true">&rarr;</span></a>
