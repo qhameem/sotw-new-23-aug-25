5c44d388c6a3bf088a7269c2daebb7b78ecb64c0
Quazi Hameem Mahmud <qhameemb@gmail.com>
2026-05-13 11:35:07 +0600

    changelog page

--- resources/views/changelogs/index.blade.php
+++ resources/views/changelogs/index.blade.php
@@ -2,15 +2,6 @@
 
 @section('title', 'Changelog')
 
-@section('actions')
-    <div class="md:flex items-center space-x-2">
-        <a href="{{ route('topics.index') }}" class="bg-white hover:bg-gray-100 text-gray-800 border border-gray-300 text-sm font-semibold py-1 px-3 rounded-md transition duration-300 shadow-sm">
-            Categories
-        </a>
-        <x-add-product-button />
-    </div>
-@endsection
-
 @section('content')
 <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
     <div class="max-w-4xl mx-auto">
