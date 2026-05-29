893170f8568dac3da583232a87563bf83c79b4fb
Quazi Hameem Mahmud <qhameemb@gmail.com>
2026-05-29 12:22:36 +0600

    cURL error fix

--- app/Jobs/SubmitIndexNowUrls.php
+++ app/Jobs/SubmitIndexNowUrls.php
@@ -14,8 +14,7 @@ class SubmitIndexNowUrls implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public int $tries = 3;
-    public int $backoff = 30;
+    public int $tries = 1;
 
     public function __construct(
         public array $urls
@@ -24,7 +23,14 @@ class SubmitIndexNowUrls implements ShouldQueue
 
     public function handle(IndexNowService $indexNow): void
     {
-        $indexNow->submitUrls($this->urls);
+        try {
+            $indexNow->submitUrls($this->urls);
+        } catch (\Throwable $exception) {
+            Log::warning('IndexNow URL submission failed.', [
+                'urls' => $this->urls,
+                'error' => $exception->getMessage(),
+            ]);
+        }
     }
 
     public function failed(\Throwable $exception): void
